e2d53525cf601febcd8fcd532521f66efe3d9985
Ayush Shukla <34869001+Aayush-Shukla@users.noreply.github.com>
2020-09-16 16:20:14 +0530

    add: show URL page

--- routes/index.js
+++ routes/index.js
@@ -1,44 +1,45 @@
 const express= require("express")
 const bodyParser=require('body-parser')
-var jsonParser = bodyParser.json()
+
 const router=express.Router()
 const Url=require('../models/urls')
-const { response } = require("express")
+
 
 
 router.post('/shorten',async function (req,res,next){
-    console.log(req.params.url,generateRandomString())
-    console.log(Url.find().or([{ "url":req.params.url }]))
+    // console.log(req.params.url,generateRandomString(),req.get('host'))
 
+    var shortened=generateRandomString()
 
     const url= new Url({
         url:req.body.url,
-        shortUrl:generateRandomString()
+        shortUrl:shortened
     })
     try{
         const tempurl=await url.save()
-        res.status(201).json(tempurl)
+
 
     }catch(err){
-        res.status(400).json({message:err.message})
+        console.log({message:err.message})
 
     }
+    res.render('short',{data:{shortUrl:shortened,hostUrl:req.get('host')}})
 
 })
 
 router.get('/:shortUrl', function (req,res,next){
 
     var short=req.params.shortUrl
-    // console.log(req.params.key,new RegExp('/'+key+'/', "i"))
 
-    let list
+
+
 
     Url.findOne().or([{"shortUrl": short}]).then(result=>{
         res.redirect(result.url)
-        res.json(result)
+
 
     }).catch(err=>{
-        res.status(400).json({message:err.message})})
+        res.render('short',{error: "Looks like you're lost"})})
 
 
 
